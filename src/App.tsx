3e8c84ce4856529ae51b3b378c81cd93103b0d8a
Tyoma <raskolnikoffweb@mail.ru>
2024-11-01 23:20:01 +0500

    second init

--- src/App.tsx
+++ src/App.tsx
@@ -9,13 +9,9 @@ function App() {
       <div></div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={() => setCount((count) => count + 1)}>asd</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs"></p>
     </>
   );
 }
